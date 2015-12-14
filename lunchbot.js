329e4b5d5bd5c22fa03f4a24d8b2920b70b16ca6
Mushfiq Mahabub <mushfiq.mahabub@ilovevideo.tv>
2015-12-14 17:46:28 +0100

    added more lunch places nearby potsdam

--- lunchbot.js
+++ lunchbot.js
@@ -42,7 +42,25 @@ var lunchSpots = [
         "location": "https://www.google.de/maps/place/Burgerb%C3%BCro/@52.39186,13.0923113,17z/data=!4m7!1m4!3m3!1s0x47a8f5f7b00cfb8f:0xc172d9cd81e73ac3!2sBurgerb%C3%BCro!3b1!3m1!1s0x47a8f5f7b00cfb8f:0xc172d9cd81e73ac3",
         "restaurant": "Burger Büro",
         "details": "Be sure to check out their <http://potsdam.redo-burgerbuero.de/|Very Pretty Website>." 
-    }
+    },
+    {
+        "location": "https://www.google.de/maps/place/SoupWorld/@52.3939748,13.0897696,17z/data=!4m6!1m3!3m2!1s0x47a8f5d98b19b8cd:0xdca88237dd6f5a83!2sSoupWorld!3m1!1s0x47a8f5d98b19b8cd:0xdca88237dd6f5a83",
+        "restaurant": "Soup World",
+        "details": "Be sure to check out their <http://www.soupworld.de/|Very Pretty Website>." 
+    },
+    {
+        "location": "https://www.google.de/maps/place/SoupWorld/@52.3939748,13.0897696,17z/data=!4m6!1m3!3m2!1s0x47a8f5d98b19b8cd:0xdca88237dd6f5a83!2sSoupWorld!3m1!1s0x47a8f5d98b19b8cd:0xdca88237dd6f5a83",
+        "restaurant": "La Copa",
+        "details": "Today's menu is <http://www.la-copa.de/texte/seite.php?id=102003|here>. Better hurry, though. They start to run out of things after 1 p.m." 
+    },
+    {
+        "location": "https://www.google.de/maps/place/NORDSEE+GmbH/@52.3922433,13.0641087,17z/data=!3m1!4b1!4m2!3m1!1s0x47a8f5c20efdef07:0x407b4ee1d149b924",
+        "restaurant": "NordSee, Potsdam Hbf"
+	},
+    {
+        "location": "http://www.yelp.de/biz/subba-vietnamesische-k%C3%BCche-potsdam",
+        "restaurant": "SuBBa Vietnamesische Küche"
+	}
 ];
 
 // Pick a number, 0 to the length of the restaurant list less one
